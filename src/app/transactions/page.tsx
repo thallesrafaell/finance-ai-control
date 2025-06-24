7026e3edf08c27ecf86359dd6b49f085f8b9df5d
thalles <thallesrafaell@outlook.com>
2025-06-24 10:19:10 -0300

    chore: add shadcn setup

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -1,5 +1,13 @@
+import { Button } from "@/components/ui/button";
+
 const TransactionPage = () => {
-  return <h1>Transaction Page</h1>;
+  return (
+    <div className="flex h-screen flex-col items-center justify-center">
+      <h1 className="mb-4 text-2xl font-bold">Transactions</h1>
+      <p className="text-gray-600">This page will display all transactions.</p>
+      <Button className="mt-4">View Transactions</Button>
+    </div>
+  );
 };
 
 export default TransactionPage;
