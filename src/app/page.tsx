4ac49e340a071ca4a2a1d169506f104a35c96283
thalles <thallesrafaell@outlook.com>
2025-06-24 11:08:37 -0300

    chore: reremove unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { UserButton } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
-import { dark } from "@clerk/themes";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
