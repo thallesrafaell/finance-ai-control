4125354145a3e8bf76d8ff21801596ff8992356a
thalles <thallesrafaell@outlook.com>
2025-06-24 10:07:04 -0300

    chore: add Prettier and Tailwind CSS plugin

--- src/app/transactions/[id]/page.tsx
+++ src/app/transactions/[id]/page.tsx
@@ -7,7 +7,7 @@ interface TransactionDetailPageProps {
 const TransactionDetailPage = ({
   params: { id },
 }: TransactionDetailPageProps) => {
-  return <h1>Transaction Detail Page {id}</h1>;
+  return <h1 className="text-2xl font-bold">Transaction Detail Page {id}</h1>;
 };
 
 export default TransactionDetailPage;
